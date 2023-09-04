9feb612edfaa3cd9bb45358ab63034610e15f30d
super-rank-dev <anthony.bartolotte.1010@gmail.com>
2023-09-04 11:30:39 -0400

    email reminder updated

--- config/key.js
+++ config/key.js
@@ -2,5 +2,5 @@ module.exports = {
     serverPort: 5000,
     mongoURI: 'mongodb://162.212.153.81:1027/prophysio_db',
     secretOrKey: '[PROPHYSIO] SECRET OR KEY',
-    serverAddress: 'localhost:3000'
+    serverAddress: 'physio2.safeguardhosting.ca'
 }
